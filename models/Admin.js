387b2c574922d79ee156c4ec52648bcb0206a454
Jeremy Cobb <jeremy.cobb@shopify.com>
2018-02-10 18:08:36 -0500

    validate user and start work for password reset flow

--- models/Admin.js
+++ models/Admin.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
-const validator = require("validator");
-const mongodbErrorHandler = require("mongoose-mongodb-errors");
-const passportLocalMongoose = require("passport-local-mongoose");
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const adminSchema = new mongoose.Schema({
   email: {
@@ -11,17 +11,19 @@ const adminSchema = new mongoose.Schema({
     unique: true,
     lowercase: true,
     trim: true,
-    validate: [validator.isEmail, "Invalid Email Address"],
-    required: "Please Supply an email address"
+    validate: [validator.isEmail, 'Invalid Email Address'],
+    required: 'Please Supply an email address'
   },
   name: {
     type: String,
-    required: "Please supply a name",
+    required: 'Please supply a name',
     trim: true
-  }
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
 adminSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 adminSchema.plugin(mongodbErrorHandler);
 
-module.exports = mongoose.model("Admin", adminSchema);
+module.exports = mongoose.model('Admin', adminSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,46 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { catchErrors } = require("../handlers/errorHandlers");
+const { catchErrors } = require('../handlers/errorHandlers');
 
-const postController = require("../controllers/postController");
-const adminController = require("../controllers/adminController");
-const authController = require("../controllers/authController");
+const postController = require('../controllers/postController');
+const adminController = require('../controllers/adminController');
+const authController = require('../controllers/authController');
 
 /* GET home page. */
-router.get("/", catchErrors(postController.loadIndex));
+router.get('/', catchErrors(postController.loadIndex));
 
-router.get("/about", function(req, res, next) {
-  res.render("page", { title: "About" });
+router.get('/about', function(req, res, next) {
+  res.render('page', { title: 'About' });
 });
 
-router.get("/admin", authController.isLoggedIn, postController.loadAdmin);
+router.get('/admin', authController.isLoggedIn, postController.loadAdmin);
 
-router.get("/admin/new-post", postController.addPost);
-router.post("/admin/new-post", catchErrors(postController.writePost));
+router.get('/admin/new-post', postController.addPost);
+router.post('/admin/new-post', catchErrors(postController.writePost));
 
-router.get("/admin/posts/:slug/edit", catchErrors(postController.editPost));
+router.get('/admin/posts/:slug/edit', catchErrors(postController.editPost));
 router.post(
-  "/admin/new-post/:slug/:id/edit",
+  '/admin/new-post/:slug/:id/edit',
   catchErrors(postController.updatePost)
 );
 
-router.get("/posts/:slug", catchErrors(postController.singlePost));
+router.get('/posts/:slug', catchErrors(postController.singlePost));
 
-router.get("/admin/login", adminController.loginForm);
+router.get('/admin/login', adminController.loginForm);
 
-router.get("/admin/logout", authController.logout);
+router.get('/admin/logout', authController.logout);
 
-router.get("/admin/register", adminController.registerForm);
+router.get('/admin/register', adminController.registerForm);
 
 router.post(
-  "/admin/register",
+  '/admin/register',
   adminController.validateAdmin,
+  adminController.handleValidatedData,
   adminController.register,
   authController.login
 );
 
-router.post("/admin/login", authController.login);
+router.post('/admin/login', authController.login);
 
+router.get('/admin/forgot', catchErrors(authController.forgotPassword));
 module.exports = router;
