88b3390d962780b038e931f610a0aa5dd7047cc4
andrewmcgov <andrew.mcgoveran@shopify.com>
2018-03-14 19:24:14 -0400

    Preview images uploading

---
+++ public/javascripts/adminModules/postForm.js
@@ -0,0 +1,63 @@
+function postForm() {
+  const featuredImgInput = document.getElementById('featuredImage');
+  const featuredImgMsg = document.querySelector('.featured-image-message');
+  const removeImgBtn = document.querySelector('.remove-featured-image');
+  const featuredThumb = document.querySelector('.featured-image-thumb');
+  const uploadImgBtn = document.querySelector('.change-featured-image');
+
+  function handleImage(file) {
+    const reader = new FileReader();
+    reader.onload = (function(img) {
+      return function(e) {
+        img.src = e.target.result;
+      };
+    })(featuredThumb);
+    reader.readAsDataURL(file);
+    featuredThumb.classList.remove('hide');
+    featuredThumb.parentElement.querySelector('hr').classList.remove('hide');
+  }
+
+  function featuredImageChange() {
+    if (featuredImgInput.files.length) {
+      featuredImgMsg.textContent = 'Save post to upload image.';
+      if (featuredThumb) featuredThumb.classList.add('hide');
+      if (removeImgBtn) removeImgBtn.classList.add('hide');
+      handleImage(featuredImgInput.files[0]);
+      uploadImgBtn.textContent = 'Change Image';
+    } else {
+      featuredImgMsg.textContent = '';
+      if (featuredThumb.dataset.hasImage) {
+        featuredThumb.src = featuredThumb.dataset.ogSrc;
+      } else {
+        if (featuredThumb) featuredThumb.classList.add('hide');
+        if (removeImgBtn) removeImgBtn.classList.remove('hide');
+        uploadImgBtn.textContent = 'Add Featured Image';
+        featuredThumb.parentElement.querySelector('hr').classList.add('hide');
+      }
+    }
+  }
+
+  function removeImage() {
+    let removeBtnData = removeImgBtn.dataset;
+    if (removeBtnData.removing === 'true') {
+      featuredThumb.classList.add('hide');
+      featuredImgMsg.textContent = 'Save post to remove image.';
+      featuredThumb.parentElement.querySelector('hr').classList.add('hide');
+      uploadImgBtn.classList.add('hide');
+      removeImgBtn.textContent = 'Cancel';
+      removeBtnData.removing = 'false';
+    } else {
+      featuredThumb.classList.remove('hide');
+      featuredImgMsg.textContent = '';
+      featuredThumb.parentElement.querySelector('hr').classList.remove('hide');
+      uploadImgBtn.classList.remove('hide');
+      removeImgBtn.textContent = 'Remove Image';
+      removeBtnData.removing = 'true';
+    }
+  }
+
+  if (removeImgBtn) removeImgBtn.addEventListener('click', removeImage);
+  featuredImgInput.addEventListener('change', featuredImageChange);
+}
+
+export default postForm;
